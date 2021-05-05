c6b42367e250b6bd147df4a762b2540413cee43a
Shelby Rothman <srothman@corelation.local>
2021-05-05 11:54:05 -0700

    rendering code to dashboard

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,5 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-export default function Dashboard() {
-    
+function Dashboard({code}) {
+
+    return <div>{code}</div>
+
 }
+
+export default Dashboard;
