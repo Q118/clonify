f28fbbc0b2b03af0fe31437e50ecee8c01a88b14
Shelby Rothman <srothman@corelation.local>
2021-05-04 21:52:26 -0700

    up to barebones login page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { useState, useEffect } from 'react'
-
-import './App.css';
+import logo from './logo.svg';
+import './App.css'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './Components/login'
 
 function App() {
-  const [resourceType, setResourceType] = useState('posts')
-
-console.log('render')
-
-useEffect(() => {
-  fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-}, [resourceType])
-
   return (
-    <>
-      <div>
-        <button onClick={() => setResourceType('posts')}>Posts</button>
-        <button onClick={() => setResourceType('users')}>Users</button>
-        <button onClick={() => setResourceType('comments')}>Comments</button>
-
-      </div>
-      <h1>{resourceType}</h1>
-    </>
+      <Login />
   );
 }
 
---
+++ src/Components/login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { Container } from 'react-bootstrap'
+
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=a3fc1a05eedf443e872b37c1fe5645c8&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+
+
+function Login() {
+  return (
+    <Container>
+        <a className="btn btn-success btn-lg" href={AUTH_URL}>
+            Login with Spotify
+        </a>
+
+    </Container>
+  );
+}
+
+export default Login;
