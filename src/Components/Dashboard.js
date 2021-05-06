51aa1f15340238d1d075676819c300f5f885bdfa
Shelby Rothman <srothman@corelation.local>
2021-05-05 20:29:00 -0700

    dashboard 44:11

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import useAuth from './useAuth'
+import { Container } from './react-bootstrap'
+import { Form } from 'react-bootstrap';
 
-function Dashboard({code}) {
+function Dashboard({ code }) {
     const accessToken = useAuth(code)
-    return <div>{code}</div>
+
+    const [search, setSearch] = useState("")
+
+    return <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
+        <Form.Control 
+            type="search"
+            placeholder="Search Songs/Artists" 
+            value={search}
+            onChange={e => setSearch(e.target.value)} 
+        />
+        <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+            {searchResults.map(track => (
+                <TrackSearchResult track={track} key={}track.uri
+            ))}
+        </div>
+    </Container>
 
 }
 
