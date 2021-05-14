23500b69b863289f57e6236de6ebed4737a373b6
Shelby Rothman <srothman@corelation.local>
2021-05-14 09:20:16 -0700

    adjusting for deployment

--- server/server.js
+++ server/server.js
@@ -10,6 +10,12 @@ app.use(cors())
 app.use(express.json({limit: '20mb'}));
 app.use(express.urlencoded({ extended: false, limit: '20mb' }));
 
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("./build"));
+  }
+
+
 app.post("/refresh", (req, res) => {
     const refreshToken = req.body.refreshToken
     const spotifyApi = new SpotifyWebApi({
